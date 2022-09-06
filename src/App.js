47e90f1a74c3d070a183f279492380c8f14d73ec
Animesh Samaiya <asamaiya00@gmail.com>
2022-09-06 23:53:21 +0530

    feat: connect wallet

--- src/App.js
+++ src/App.js
@@ -3,16 +3,33 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap';
 
 function App() {
+  const connectWallet = async () => {
+    if (!window.ethereum) {
+      alert('Please install Metamask wallet');
+      return;
+    }
+    const accounts = await window.ethereum.request({
+      method: 'eth_requestAccounts',
+    });
+    console.log(accounts);
+  };
+
   return (
     <div className="container">
       <div className="row justify-content-center text-center mt-5">
         <form className="col-lg-6 shadow p-3 mb-5 bg-white rounded">
           <h4>Mint Portal</h4>
           <h5>Please connect your wallet</h5>
-          <Button>Connect Wallet</Button>
+          <Button onClick={connectWallet}>Connect Wallet</Button>
           <div className="my-3 card gap-3">
             <label>Wallet Address</label>
-            <input className="mx-2" type="number" defaultValue={1} min={1} max={5} />
+            <input
+              className="mx-2"
+              type="number"
+              defaultValue={1}
+              min={1}
+              max={5}
+            />
             <label>Please select the number of NFTs to mint</label>
             <Button>Mint</Button>
           </div>
