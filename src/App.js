0190b01d6056760f4cb01ff25f307e41055d4060
Animesh Samaiya <asamaiya00@gmail.com>
2022-09-16 02:07:35 +0530

    fix: webpack downgrade & contract setup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
 import { Button } from 'react-bootstrap';
+import ABI from './ABI.json';
+import Web3 from 'web3';
+
+const ADDRESS = '0x6133152377a9dbbe7b9e812f514f884d59e2b0d1';
 
 function App() {
+  const [address, setAddress] = useState('');
   const connectWallet = async () => {
     if (!window.ethereum) {
       alert('Please install Metamask wallet');
       return;
     }
-    const accounts = await window.ethereum.request({
-      method: 'eth_requestAccounts',
-    });
-    console.log(accounts);
+    let web3 = new Web3(window.ethereum);
+    await window.ethereum.request({ method: 'eth_requestAccounts' });
+    const accounts = await web3.eth.getAccounts();
+    // console.log(accounts);
+    setAddress(accounts[0]);
+
+    const contract = new web3.eth.Contract(ABI, ADDRESS)
+    console.log(contract)
   };
 
   return (
@@ -20,9 +30,9 @@ function App() {
         <form className="col-lg-6 shadow p-3 mb-5 bg-white rounded">
           <h4>Mint Portal</h4>
           <h5>Please connect your wallet</h5>
-          <Button onClick={connectWallet}>Connect Wallet</Button>
+          {!address && <Button onClick={connectWallet}>Connect Wallet</Button>}
           <div className="my-3 card gap-3">
-            <label>Wallet Address</label>
+            <label>{address ? address : 'Wallet Address'}</label>
             <input
               className="mx-2"
               type="number"
