ef27987c7106d5e81bd37b8d98440345768f258b
Animesh Samaiya <asamaiya00@gmail.com>
2022-09-19 23:45:55 +0530

    feat: fetch tokens minted

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import { Button } from 'react-bootstrap';
 import ABI from './ABI.json';
 import Web3 from 'web3';
+import axios from 'axios';
 
-const ADDRESS = '0x38ac452166A993A4031172c6802923DaA4215796';
+const CONTRACT_ADDRESS = '0x38ac452166A993A4031172c6802923DaA4215796';
+const BASE_URL = 'https://api-rinkeby.etherscan.io/api';
 
 function App() {
   const mintAmountRef = useRef(1);
   const [address, setAddress] = useState('');
   const [contract, setContract] = useState(null);
+  const [tokensMinted, setTokensMinted] = useState(null);
+
+  useEffect(() => {
+    window.process = {
+      ...window.process,
+    };
+    fetchTokensMinted();
+  }, []);
+
+  const fetchTokensMinted = async () => {
+    const response = await axios.get(
+      BASE_URL +
+        `?module=stats&action=tokensupply&contractaddress=${CONTRACT_ADDRESS}&apikey=${process.env.REACT_APP_API_KEY}`
+    );
+    console.log(response.data.result);
+    setTokensMinted(response.data.result);
+  };
+
   const connectWallet = async () => {
     if (!window.ethereum) {
       alert('Please install Metamask wallet');
@@ -21,7 +41,7 @@ function App() {
     const accounts = await web3.eth.getAccounts();
     setAddress(accounts[0]);
 
-    setContract(new web3.eth.Contract(ABI, ADDRESS));
+    setContract(new web3.eth.Contract(ABI, CONTRACT_ADDRESS));
   };
 
   const mint = async () => {
@@ -52,11 +72,12 @@ function App() {
               defaultValue={1}
               min={1}
               max={5}
-            /> 
+            />
             <label>Please select the number of NFTs to mint (1-5)</label>
             <Button onClick={mint}>Mint</Button>
           </div>
           <label>Price 0.05 ETH each mint</label>
+          <h5 className='mt-2'>Tokens Minted {tokensMinted}/1000</h5>
         </form>
       </div>
     </div>
